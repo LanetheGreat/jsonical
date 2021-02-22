9da5d68aa216b38dd1515e429a1edf76a85f4719
Lane Shaw <lanethegreat@gmail.com>
2021-02-22 10:12:30 -0500

    Updated setup for new setuptools and jsonical for Python 3.

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@
 # This file is part of the nebgb package released under the MIT license.
 #
 
-import os
-from distutils.core import setup
+from setuptools import setup
 
 long_desc = """
 Canonical serializations for JSON.
@@ -14,18 +13,19 @@ Similar to the basic API for the `json`/`simplejson` modules with
 a few restrictions on modifiying serialization.
 """
 
-
 setup(
-    name = "jsonical",
-    description = "Canonical JSON",
-    long_description = long_desc,
-    author = "Paul Joseph Davis",
-    author_email = "paul.joseph.davis@gmail.com",
-    url = "http://github.com/davisp/jsonical",
-    version = "0.0.4",
-    license = "MIT",
-    keywords = "JSON canonical serialization",
-    platforms = ["any"],
-    zip_safe = True,
-    py_modules = ["jsonical"]
+    name="jsonical3",
+    description="Canonical JSON for Python 3",
+    long_description=long_desc,
+    author="Paul Joseph Davis",
+    author_email="paul.joseph.davis@gmail.com",
+    maintainer="Lane Shaw",
+    maintainer_email="lshaw.tech@gmail.com",
+    url="https://github.com/LanetheGreat/jsonical3",
+    version="0.1.0",
+    license="MIT",
+    keywords="JSON canonical serialization",
+    platforms=["any"],
+    zip_safe=True,
+    py_modules=["jsonical3"],
 )
